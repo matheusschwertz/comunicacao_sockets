ebc5ebdcaeed21a7f0fa9f16612c4c4462c0da5f
matheusschwertz <matheusschwertz@gmail.com>
2023-04-02 23:34:11 -0300

    alteracao 1

---
+++ server.py
@@ -0,0 +1,19 @@
+#!/user/bin/env python3
+
+import socket
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+socket.bind(("localhost", 9473))
+socket.listen()
+
+conn, addr = socket.accept()
+print(addr)
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print("Nova mensagem do host %s: %s" % (addr, data.decode()))
+
+conn.close()
